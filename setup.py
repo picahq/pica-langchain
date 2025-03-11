8233145c62851dd4b5107760f2bb06e67708d902
Peter Kuczera <peter@picaos.com>
2025-03-11 14:56:30 -0400

    Bump package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pica-langchain",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=[
         "langchain==0.3.20",
