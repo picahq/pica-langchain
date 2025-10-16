e249cf65533572221f52463281dfcfdbd33bb24d
Paul K. <paul@paulkr.com>
2025-10-16 12:25:39 -0400

    chore: bump version to 2.1.0 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pica-langchain",
-    version="2.0.1",
+    version="2.1.0",
     packages=find_packages(),
     install_requires=[
         "langchain>=0.3.25,<1.0.0",
